45cbd9017a82463f8533aef0958b48fa7700ec36
Gallone <iankirk@live.com>
2021-01-11 18:01:57 +1100

    Barrage Missile Auto Fire and Spread

---
+++ Assets/Scripts/BarrageMissile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrageMissile : MonoBehaviour
+{
+    public float speed = 3;
+    public float rotationSpeed = 120;
+
+    public float randomTimer = 0;
+    public int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomTimer = Random.Range(0.1f, 0.3f);
+        direction = (1 - (Random.Range(0, 2) * 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        randomTimer -= Time.deltaTime;
+        if(randomTimer <= 0)
+        {
+            randomTimer = Random.Range(0.1f, 0.3f);
+            direction *= -1;
+        }
+        transform.eulerAngles += new Vector3(0, 0, direction * rotationSpeed * Time.deltaTime);
+        transform.position += transform.up * speed * Time.deltaTime;
+        if(transform.position.y >= 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HumanController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    public enum ABILITY {MISSILE, HEAT, SPEED, UBER};
+    public ABILITY mode = ABILITY.MISSILE;
+
+    public float speed = 5;
+
+    public GameObject barrageMissile;
+    float BMTimer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0);
+        if (mode == ABILITY.MISSILE)
+        {
+            BMTimer += Time.deltaTime;
+            if (BMTimer >= 0.2f)
+            {
+                BMTimer -= 0.2f;
+                Instantiate(barrageMissile, transform.position + new Vector3(Random.Range(-0.2f, 0.2f), 0), transform.rotation);
+            }
+        }
+    }
+}
